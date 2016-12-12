067b403b1a91fd4c0d6970a0a1a1aeca11ae3e2c
mattroz <matveyrozanov@gmail.com>
2016-12-12 23:42:13 +0300

    defined main heap functions, all functions will return values by 'parameteres'

--- sources/headers/heap.h
+++ sources/headers/heap.h
@@ -1,7 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef HEAP_H
+#define HEAP_H
 
-typedef struct Heap_node
-{
-	
-}head_node;
+#include "node.h"
+
+int heap_initialize(heap_t, int);
+int heap_swap(heapnode_t*, heapnode_t*);
+int deallocate_heap(heap_t*);
+int heap_insert(heap_t, node_t*, int);
+int heap_get_min(heap_t*, node_t*);
+int get_parent(heap_t*, int);
+int get_rchild(heap_t*, int);
+int get_lchild(heap_t*, int);
+
+#endif
